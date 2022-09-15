7f8c446b1499989800eef1c94ec377e65be76bef
Shubham <yadav105shubham@gmail.com>
2022-09-16 00:12:10 +0530

    initial commit

---
+++ website/layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+:root {
+  --nav-primary: #1d1283;
+  --nav-secondary: #49a82e;
+  --color-white: #ffffff;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+li {
+  list-style: none;
+}
+button {
+  color: inherit;
+}
+</style>
